c3467a1143f619d60bbc97956207f6f6da489c4e
integral_digi <ojaduagreat@gmail.com>
2024-03-28 17:17:00 +0100

    personal settings fix

--- app/account/components/ProfileTab.tsx
+++ app/account/components/ProfileTab.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { Tab } from "@headlessui/react";
 import PersonalSettings from "./Personal";
+import { user } from "./UserData";
 
-const tabStyle = "whitespace-nowrap overflow-x-scroll px-2 py-1 h-12 font-bold ui-selected:rounded-md ui-selected:px-4 ui-selected:bg-slate-800 ui-selected:text-indigo-800 text-white";
+const tabStyle = "whitespace-nowrap px-2 py-1 h-12 font-bold ui-selected:rounded-md ui-selected:px-4 ui-selected:bg-slate-800 ui-selected:text-indigo-800 text-white";
 
 const ProfileTab = () => {
     return (
@@ -14,20 +15,20 @@ const ProfileTab = () => {
                     </Tab>
                     <Tab className={tabStyle}>Withdrawal Details</Tab>
                     <Tab className={tabStyle}>Security Settings</Tab>
-                    <Tab className={tabStyle}>Documents</Tab>
+                    <Tab className={tabStyle}>KYC</Tab>
                 </Tab.List>
                 <Tab.Panels className="font-bold w-full pt-6 sm:mx-auto sm:w-[338px] pr-8 lg:pr-0">
                     <Tab.Panel>
-                        <PersonalSettings />
+                        <PersonalSettings user={user} />
                     </Tab.Panel>
                     <Tab.Panel>
-                        <PersonalSettings />
+                        <PersonalSettings user={user} />
                     </Tab.Panel>
                     <Tab.Panel>
-                        <PersonalSettings />
+                        <PersonalSettings user={user} />
                     </Tab.Panel>
                     <Tab.Panel>
-                        <PersonalSettings />
+                        <PersonalSettings user={user} />
                     </Tab.Panel>
                 </Tab.Panels>
             </Tab.Group>
